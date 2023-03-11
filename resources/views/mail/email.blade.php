7c4fe74b5dde67d56d4b820af482bca4b8cb272a
sharif1811 <sharifhossaion57@gmail.com>
2023-03-11 12:35:27 +0600

    part 3

---
+++ resources/views/mail/email.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Email Notifaciton</title>
+</head>
+<body>
+    <h1>Hellow</h1>
+    <h3>Welcome to our another page</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur expedita consequatur voluptatum nesciunt? Eaque, aliquam consequuntur in quisquam vero commodi consequatur magni, eos assumenda animi obcaecati, quidem exercitationem maiores quo.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/send-notification',function(){
-    $user = User::find(2);
-   // $user->notify(new EmailNotification());
-   Notification::send($user, new EmailNotification());
+//     $user = User::find(2);
+//    $user->notify(new EmailNotification());
+//    Notification::send($user, new EmailNotification());
+//     return redirect()->back();
+
+    $users = User::all();
+    foreach($users as $user){
+      Notification::send($user, new EmailNotification());
+
+    }
     return redirect()->back();
+
 });
 
 Auth::routes();
